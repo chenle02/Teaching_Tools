0cea87ec4b2802212587176fd4a03cf1449ea9c9
chenle02 <chenle02@gmail.com>
2021-03-18 11:57:18 -0400

    Add one script to set up directories for a new course.

---
+++ scripts/SetupCouseDirecotory.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Run the following commands in the home directory of the course
+mkdir Textbook
+mkdir vimwiki
+mkdir vimwiki_html
+mkdir slides
+mkdir Homework
+mkdir Announcement
+mkdir Misc
+mkdir Scores
+cp ~/Dropbox/Teaching/svn_teaching/2021_Spring_Math221/upload .
+cp ~/Dropbox/Teaching/svn_teaching/2021_Spring_Math221/wordcloud.tex .
+cp ~/Dropbox/Teaching/svn_teaching/2021_Spring_Math221/gen_wordcloud.sh .
+cp ~/Dropbox/Teaching/svn_teaching/2021_Spring_Math221/weights.r .
